4b7e4002e739640b73169f66ee361402286f24b8
Roman Isaikin <romix.lab@gmail.com>
2025-05-27 22:58:29 +0200

    feat: generate build.rs

--- bedrock_build/src/lib.rs
+++ bedrock_build/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::ffi::OsString;
+use std::{env, fs};
+use std::path::PathBuf;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn common() {
+    println!("cargo:rustc-linker=flip-link");
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    if env::var_os("RAM_LINK") == Some(OsString::from("1")) {
+        fs::write(
+            out.join("link_ram.x"),
+            include_bytes!("../link_ram_cortex_m.x"),
+        )
+            .unwrap();
+        println!(
+            "cargo::warning=⚠️ \x1b[1;33mUsing RAM linking, old code will be run from FLASH on power-cycle"
+        );
+        println!("cargo:rustc-link-arg=-Tlink_ram.x");
+    } else {
+        println!("cargo:rustc-link-arg=-Tlink.x"); // provided by cortex-m-rt
     }
+
+    println!("cargo:rustc-link-arg=-Tdefmt.x");
+    
+    // This is needed if your flash or ram addresses are not aligned to 0x10000 in memory.x
+    // See https://github.com/rust-embedded/cortex-m-quickstart/pull/95
+    println!("cargo:rustc-link-arg=--nmagic");
+    
+    println!("cargo:rerun-if-changed=../link_ram_cortex_m.x");
 }
